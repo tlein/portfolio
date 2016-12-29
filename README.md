a8456449b38d320b1b113b94fddf3ca65c7ef9b7
tlein <tlein@radford.edu>
2016-12-28 17:59:29 -0700

    integrate pug with the project

---
+++ README.md
@@ -0,0 +1,19 @@
+# Tucker Lein's Portfolio
+
+## About
+
+My personal portfolio made with Jekyll
+
+## Setup Instructions
+
+### Dependencies
+
+* Python3
+* [pug-cli](https://github.com/pugjs/pug-cli)
+* [jekyll](https://jekyllrb.com/docs/quickstart/)
+
+### Serving local server from command line
+
+Included is a python3 script that will serve the jekyll in a local server (http://localhost:4000/). 
+
+`./serve.py`
---
+++ serve.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+from __future__ import print_function
+
+import subprocess
+import signal
+import sys
+
+JEKYLL_PROCESS = subprocess.Popen(["jekyll", "serve"])
+subprocess.Popen("pug -w -o _layouts/ _layouts/pug/*.pug", shell=True)
+
+def crash_handler(signal, frame):
+    """Handles Ctrl+C crash to kill the jekyll process"""
+    print("\nGoodbye")
+    JEKYLL_PROCESS.kill()
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, crash_handler)
+signal.pause()
