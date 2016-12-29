f9c83a96916b8fa894e3bc29158ddf229ca5557c
tlein <tlein@radford.edu>
2016-12-28 18:05:33 -0700

    kill pug process when serve script ends

--- serve.py
+++ serve.py
@@ -7,12 +7,13 @@ import signal
 import sys
 
 JEKYLL_PROCESS = subprocess.Popen(["jekyll", "serve"])
-subprocess.Popen("pug -w -o _layouts/ _layouts/pug/*.pug", shell=True)
+PUG_PROCESS = subprocess.Popen("pug -w -o _layouts/ _layouts/pug/*.pug", shell=True)
 
 def crash_handler(signal, frame):
     """Handles Ctrl+C crash to kill the jekyll process"""
     print("\nGoodbye")
     JEKYLL_PROCESS.kill()
+    PUG_PROCESS.kill()
     sys.exit(0)
 
 signal.signal(signal.SIGINT, crash_handler)
